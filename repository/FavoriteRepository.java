8cdf031c6b67172e6f6091d2c3c64714978fbf56
ddqq4 <dariab51261@gmail.com>
2026-01-18 17:17:00 +0400

    FavoriteRepository.java

--- repository/FavoriteRepository.java
+++ repository/FavoriteRepository.java
@@ -1,10 +1,7 @@
 package ru.socialmap.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.socialmap.model.Favorite;
-
 import java.util.List;
-
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     List<Favorite> findByUserId(Long userId);
     Favorite findByUserIdAndPointId(Long userId, Long pointId);
