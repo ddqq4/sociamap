14bf9a637aa02b847cd908f5c01ac449b0df02e3
ddqq4 <dariab51261@gmail.com>
2026-01-18 17:17:53 +0400

    SecurityConfig.java

--- security/SecurityConfig.java
+++ security/SecurityConfig.java
@@ -1,13 +1,10 @@
 package ru.socialmap.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -19,7 +16,6 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable());
-
         return http.build();
     }
 }
